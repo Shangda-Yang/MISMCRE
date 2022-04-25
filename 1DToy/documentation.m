9af5e154cf2ad8bb93e9e3195bb4c4f72f027942
Shangda-Yang <84387547+Shangda-Yang@users.noreply.github.com>
2022-04-25 10:25:41 +0100

    Update documentation.m

--- 1DToy/documentation.m
+++ 1DToy/documentation.m
@@ -6,13 +6,6 @@
 %   * Noise N(0,0.2^2) of statistical model
 %   * Quantity of interest: x^2 x ~ \pi(dx) (posterior)
 %   * 10 observations on linspace(0, 1, 10)
-%   * Emperical values are computed with N = 10000 and 
-%     M = 100 (realisations) and regression on L = 2:6
-%   * MLSMC and SMC are implemented with M = 100
-%     SMC sampler tempering step \lambda = 0.5
-%     MCMC transition kernel RWM \beta = 0.5 and MCMC step = 8
-%   * MLMCMC and MCMC are implemented with M = 100
-%     MCMC transition kernel RWM \beta = 0.8
 % ------------------------------------------------------------- %
 
 % ------------------------------------------------------------- %
