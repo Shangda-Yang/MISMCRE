03f7390b02b8a145400b75693b6ce726f99a66ae
Shangda-Yang <84387547+Shangda-Yang@users.noreply.github.com>
2022-06-30 19:37:25 +0100

    Update runMLMCMCtest.m

--- 1DToy/MLMCMC/runMLMCMCtest.m
+++ 1DToy/MLMCMC/runMLMCMCtest.m
@@ -16,7 +16,7 @@ Eps    = 2.^(-11:1:-4);
 % generate data
 x_data = linspace(0, 1, 10); % corresponding point of data
 sigma  = 0.2;
-data   = -50*rand(1)*(x_data.^2-x_data) + sigma*randn(length(x_data),1);
+data   = -0.5*rand(1)*(x_data.^2-x_data) + sigma*randn(length(x_data),1);
 
 filename = 'Toy_Example_MLMCMC';
 fp = fopen([filename '.txt'],'w');
