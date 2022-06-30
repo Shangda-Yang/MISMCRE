1d510a787c5da8f9f1368ebf093b64bde0a126f4
Shangda-Yang <84387547+Shangda-Yang@users.noreply.github.com>
2022-06-30 19:36:55 +0100

    Update runMLSMCtest.m

--- 1DToy/MLSMC/runMLSMCtest.m
+++ 1DToy/MLSMC/runMLSMCtest.m
@@ -19,7 +19,7 @@ Eps    = [ 0.0025 0.005 0.01 0.02 0.04 ];
 % generate data
 x_data = linspace(0, 1, 10); % corresponding point of data
 sigma  = 0.2;
-data   = -50*rand(1)*(x_data.^2-x_data) + sigma*randn(length(x_data),1);
+data   = -0.5*rand(1)*(x_data.^2-x_data) + sigma*randn(length(x_data),1);
 
 filename = 'Toy_Example_MLSMC';
 fp = fopen([filename '.txt'],'w');
